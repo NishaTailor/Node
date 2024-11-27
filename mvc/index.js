42cb62f3847a585be1fe38e63f20bb546b8feda4
Nisha Tailor <tailornisha26@gmail.com>
2024-11-27 15:48:01 +0530

    Node

--- mvc/index.js
+++ mvc/index.js
@@ -3,7 +3,7 @@ const dbConnect = require("./config/db");
 const app = express()
 
 app.use(express.json())
-app.use("/",userRouter)
+// app.use("/", userRouter)
 app.listen(3500, () => {
     console.log("listening on http://localhost:3500");
     dbConnect()
